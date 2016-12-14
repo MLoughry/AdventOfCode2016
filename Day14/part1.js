c588660ff4b370126a4c2af4ee4874639c490e1f
Michael Loughry <miclo@microsoft.com>
2016-12-14 13:37:15 -0800

    Day 14

--- Day14/part1.js
+++ Day14/part1.js
@@ -1,6 +1,6 @@
 const md5 = require('md5');
 
-var input = 'abc';
+var input = 'zpqevtbw';
 
 var potentialKeys = [];
 var confirmedKeys = [];
@@ -14,12 +14,15 @@ for (var i = 0; i < stopHashIndex; i++) {
     if (match = tripsRegex.exec(hash)) {
         potentialKeys.push({index: i, char: match[1], hash: hash});
 
+        // Can only be 5-of-a-kind if it's also 3-of-a-kind
         if (match = quintRegex.exec(hash)) {
-            // Clear out 'expired' keys
+            // Take this opportunity to clear out 'expired' keys
             while (potentialKeys[0].index < (i - 1000)) {
                 potentialKeys.shift();
             }
-            for (var j = 0; j < potentialKeys.length; j++) {
+
+            // Check all potential keys to see if they're confirmed
+            for (var j = 0; j < potentialKeys.length - 1; j++) {
                 var quintChar = new RegExp(`${potentialKeys[j].char}{5}`);
                 if (quintChar.test(hash)) {
                     var confirmed = potentialKeys[j];
@@ -28,6 +31,9 @@ for (var i = 0; i < stopHashIndex; i++) {
                     confirmedKeys.push(confirmed);
                     potentialKeys.splice(j, 1);
                     j--;
+
+                    // If we've found 64 confirmed keys, then cycle through the next 1000 hashes
+                    //   to shake out all the remaining potential keys.
                     if (confirmedKeys.length === 64) {
                         stopHashIndex = i + 1000;
                     }
@@ -37,8 +43,10 @@ for (var i = 0; i < stopHashIndex; i++) {
     }
 }
 
+// Sort the confirmed keys by their indices
 confirmedKeys = confirmedKeys.sort((a,b) => a.index - b.index);
 
+// Validate that all keys are valid, since I seem to have too many
 confirmedKeys.forEach(key => {
     if (key.index < key.confirmedIndex - 1000
         || !(new RegExp(`${key.char}{5}`).test(key.confirmedHash))) {
@@ -46,4 +54,5 @@ confirmedKeys.forEach(key => {
         }
 })
 
+// This should be the answer
 console.log(confirmedKeys[63].index);
